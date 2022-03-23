e68cc478019c7e78500acd2a8022f65b09724ede
DevKnow <helio524@naver.com>
2022-03-23 23:48:13 +0900

    Item1_23page 쿼리 결과 반환
    Item1_23page 쿼리 결과 반환

--- Item1_18page/Item1_18page/Program.cs
+++ Item1_18page/Item1_18page/Program.cs
@@ -1,7 +1,40 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
 
 namespace Item1_18page
 {
+    public class DB
+    {
+        public List<CustomerInfo> Customers;
+
+        public DB()
+        {
+            Customers = new List<CustomerInfo>();
+        }
+
+        public void Init()
+        {
+            Customers.Add(new CustomerInfo("김민지", "010-444-444"));
+            Customers.Add(new CustomerInfo("이은비", "010-555-555"));
+            Customers.Add(new CustomerInfo("빅육육", "010-666-666"));
+            Customers.Add(new CustomerInfo("김칠팔", "010-777-888"));
+        }
+    }
+
+    public class CustomerInfo
+    {
+        public string ContactName;
+        public string ContactNumber;
+
+        public CustomerInfo(string name, string number)
+        {
+            ContactName = name;
+            ContactNumber = number;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
@@ -13,7 +46,17 @@ namespace Item1_18page
 
             var f = foo.GetMagicNumber();
             var total = 100 * f / 6;
-            Console.WriteLine($"Declared Type: {total.GetType().Name}, Value: {total}");
+            // Console.WriteLine($"Declared Type: {total.GetType().Name}, Value: {total}");
+
+            var test = new DB();
+            test.Init();
+
+            var list = foo.FindCustomersStartingWith(test, "김");
+
+            foreach(var c in list)
+            {
+                Console.WriteLine(c);
+            }
         }
     }
 
@@ -22,6 +65,17 @@ namespace Item1_18page
         public double GetMagicNumber()
         {
             return 10.0;
+        }
+
+        public IEnumerable<string> FindCustomersStartingWith(DB db, string start)
+        {
+            var q =
+                from c in db.Customers
+                select c.ContactName;
+
+            var q2 = q.Where(s => s.StartsWith(start));
+
+            return q2;
         }
     }
 
@@ -37,6 +91,6 @@ namespace Item1_18page
     // 팩토리 메소드 확인용
     public class SavingsAccount
     {
-
+        
     }
 }

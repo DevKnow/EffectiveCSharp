271741c9151c7e2ebecdbcd07fe9492949214ab3
DevKnow <helio524@naver.com>
2022-03-23 22:41:09 +0900

    Item1_18page
    Item1_18page

---
+++ Item1_18page/Item1_18page/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Item1_18page
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

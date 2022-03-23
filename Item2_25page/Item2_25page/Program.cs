090b2630c13c4d469afdc4c590a52051c152feb1
DevKnow <helio524@naver.com>
2022-03-24 00:44:00 +0900

    Item2_25page
    Item2_25page

---
+++ Item2_25page/Item2_25page/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Item2_25page
+{
+    class Program
+    {
+        static readonly YearList meme1 = new YearList(1,2,3);
+        static readonly YearList meme2 = new YearList(500, 3, 4);
+
+        static void Main(string[] args)
+        {
+            meme1.Print();
+            meme2.Print();
+
+            if (DateTime.Now.Year.Equals(YearList.Millenium))
+                Console.WriteLine("현재는 밀레니엄!");
+
+            if (DateTime.Now.Year.Equals(YearList.ThisYear))
+                Console.WriteLine("현재는 2022년!");
+        }
+    }
+
+    public class YearList
+    {
+        // 컴파일 상수
+        public const int Millenium = 2000;
+
+        // 런타임 상수
+        public static readonly int ThisYear = 2022;
+
+        int a, b, c;
+
+        public YearList(int one, int two, int three)
+        {
+            a = one;
+            b = two;
+            c = three;
+        }
+
+        public void Print()
+        {
+            Console.Write(a+" ");
+            Console.Write(b + " ");
+            Console.WriteLine(c);
+        }
+    }
+}

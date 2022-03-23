d4fb7f061e9bb6bbc9a8d18f80cca51e4d6401e6
DevKnow <helio524@naver.com>
2022-03-23 23:02:52 +0900

    Item1_18page 잘못 저장한 파일 재저장
    Item1_18page 잘못 저장한 파일 재저장

--- Item1_18page/Item1_18page/Program.cs
+++ Item1_18page/Item1_18page/Program.cs
@@ -6,7 +6,37 @@ namespace Item1_18page
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var foo = new MyType();
+
+            AccountFactory factory = new AccountFactory();
+            var thing = factory.CreateSavingsAccount();
+
+            var f = foo.GetMagicNumber();
+            var total = 100 * f / 6;
+            Console.WriteLine($"Declared Type: {total.GetType().Name}, Value: {total}");
         }
+    }
+
+    public class MyType
+    {
+        public double GetMagicNumber()
+        {
+            return 10.0;
+        }
+    }
+
+    // 팩토리 메소드 확인용
+    public class AccountFactory
+    {
+        public SavingsAccount CreateSavingsAccount()
+        {
+            return new SavingsAccount();
+        }
+    }
+
+    // 팩토리 메소드 확인용
+    public class SavingsAccount
+    {
+
     }
 }

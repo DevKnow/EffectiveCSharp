fb8f1d9fd5b37639e392d1522895a004516f504f
DevKnow <helio524@naver.com>
2022-03-25 22:52:33 +0900

    Item6_46page
    Item6_46page

---
+++ Item6_46page/Item6_46page/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Item6_46page
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                object? a=null;
+                ExceptionMessgae(a);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public static void ExceptionMessgae(object thisCantBeNull)
+        {
+            if(thisCantBeNull==null)
+                throw new ArgumentNullException(nameof(thisCantBeNull), "We told you this cant be null");
+        }
+    }
+}

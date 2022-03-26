00c8373ad910c3ed79d7ced0b4703e1c3485fdab
DevKnow <helio524@naver.com>
2022-03-27 02:51:49 +0900

    Item15_88page
    Item15_88page

---
+++ Item15_88page/Item15_88page/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+
+namespace Item15_88page
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder msg = new StringBuilder("Hello, ");
+
+            msg.Append("thisUser.Name");
+            msg.Append(". Today is ");
+            msg.Append(DateTime.Now.ToString());
+
+            string finalMsg = msg.ToString();
+
+            Console.WriteLine(finalMsg);
+        }
+    }
+}

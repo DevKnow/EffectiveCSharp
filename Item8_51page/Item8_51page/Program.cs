b824e18a0102ddc3b9c3488bb5f97957944cc02a
DevKnow <helio524@naver.com>
2022-03-26 19:13:46 +0900

    Item8_51page
    Item8_51page

---
+++ Item8_51page/Item8_51page/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Item8_51page
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    public class EventSource
+    {
+        private EventHandler<int> Update;
+
+        public void RaiseUpdates()
+        {
+            counter++;
+            //var handler = Update;
+            //if(handler!=null)
+            //    handler(this, counter);
+            Update?.Invoke(this, counter);
+        }
+
+        private int counter = 0;
+    }
+}

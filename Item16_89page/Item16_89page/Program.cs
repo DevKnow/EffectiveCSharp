58858df8c9a601fb39255036650723ac9c2955a7
DevKnow <helio524@naver.com>
2022-03-27 03:33:20 +0900

    Item16_89page
    Item16_89page

---
+++ Item16_89page/Item16_89page/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Item16_89page
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Set by initializer가 출력됨
+            var d = new Derived("Constructed in main");
+        }
+    }
+
+    public class B
+    {
+        protected B()
+        {
+            VFunc();
+        }
+
+        protected virtual void VFunc()
+        {
+            Console.WriteLine("VFunc in B");
+        }
+    }
+
+    public class Derived : B
+    {
+        private readonly string msg = "Set by initializer";
+
+        public Derived(string msg)
+        {
+            this.msg = msg;
+        }
+
+        protected override void VFunc()
+        {
+            Console.WriteLine(msg);
+        }
+    }
+}

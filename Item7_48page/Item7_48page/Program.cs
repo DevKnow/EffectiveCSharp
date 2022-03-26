9679147b39fc350844fb50754d7bbd0d65f4da47
DevKnow <helio524@naver.com>
2022-03-26 18:47:49 +0900

    Item7_48page
    Item7_48page

---
+++ Item7_48page/Item7_48page/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Item7_48page
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Enumerable.Range(1, 200).ToList();
+
+            var oddNumbers = numbers.Find(n => (n % 2).Equals(1));
+            var test = numbers.TrueForAll(n => n < 50);
+
+            test = numbers.TrueForAll(n => (n % 2).Equals(1));
+            Console.WriteLine(test);
+
+            numbers.RemoveAll(n => (n % 2).Equals(0));
+            numbers.ForEach(item => Console.WriteLine(item));
+
+            test = numbers.TrueForAll(n => (n % 2).Equals(1));
+            Console.WriteLine(test);
+        }
+    }
+}

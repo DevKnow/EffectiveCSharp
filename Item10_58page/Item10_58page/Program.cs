7cb3921b2a33b5e3b716ddeed7b0b089210cccbd
DevKnow <helio524@naver.com>
2022-03-26 23:34:35 +0900

    Item10_58page
    Item10_58page

---
+++ Item10_58page/Item10_58page/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Item10_58page
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object c = MakeObject();
+
+            MyClass cl = c as MyClass;
+            cl?.MagicMethod();
+
+            MyOtherClass cl2 = c as MyOtherClass;
+            cl2?.MagicMethod();
+        }
+
+        public static object MakeObject()
+        {
+            return new object();
+        }
+    }
+
+    public class MyClass : Object
+    {
+        public void MagicMethod()
+        {
+            Console.WriteLine("MyClass 클래스의 MagicMethod() 호출합니다");
+        }
+    }
+
+    public class MyOtherClass : MyClass
+    {
+        public new void MagicMethod()
+        {
+            Console.WriteLine("MyOtherClass 클래스의 MagicMethod() 호출합니다");
+        }
+    }
+}
